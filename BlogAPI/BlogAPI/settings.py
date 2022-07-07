8f094fbe7326c95864fbf89b20ad621412f689c2
Alangobe <alangobe01@gmail.com>
2022-07-07 18:54:40 -0500

    Instalado drf-yasg

--- BlogAPI/BlogAPI/settings.py
+++ BlogAPI/BlogAPI/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
     'Users',
 ]
 
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static'
 
 AUTH_USER_MODEL = 'Users.User'
 
--- BlogAPI/BlogAPI/urls.py
+++ BlogAPI/BlogAPI/urls.py
@@ -13,9 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BlogAPI",
+        default_version='v1',
+        description='Documentación de la API del blog :D',
+        terms_of_service='',
+        contact=openapi.Contact(email='alangobe01@gmail.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    # permissions_classes=(permissions.AllowAny),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
