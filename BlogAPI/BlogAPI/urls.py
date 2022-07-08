dd13b414e617817f562196ad06fba784c388510b
Alangobe <alangobe01@gmail.com>
2022-07-07 19:33:55 -0500

    Creado serializador y vistas de usuario

--- BlogAPI/BlogAPI/urls.py
+++ BlogAPI/BlogAPI/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -36,4 +36,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('Users.urls'))
 ]
--- BlogAPI/Users/views.py
+++ BlogAPI/Users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+class RegisterView(APIView):
+    
+    def post(self, request):
+        
+        serializer = UserSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
