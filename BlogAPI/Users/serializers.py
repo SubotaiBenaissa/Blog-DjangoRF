27ea788fbe4d5756e7cc32bf535551e9bb2dbf48
Alangobe <alangobe01@gmail.com>
2022-07-20 11:04:50 -0500

    Terminado auth y login de usuario y creada app categorías

--- BlogAPI/Users/serializers.py
+++ BlogAPI/Users/serializers.py
@@ -26,3 +26,10 @@ class UserSerializer(serializers.ModelSerializer):
         
         model= User
         fields= ['id', 'email', 'username', 'first_name', 'last_name']
+        
+class UserUpdateSerializer(serializers.ModelSerializer):
+    
+    class Meta: 
+        
+        model = User
+        fields = ['first_name', 'last_name']
