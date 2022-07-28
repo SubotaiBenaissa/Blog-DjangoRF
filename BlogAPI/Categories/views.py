d352a0d47eaa28b79717f2fb06063e94d80c0e17
Alangobe <alangobe01@gmail.com>
2022-07-27 22:58:48 -0500

    Configurados permisos

--- BlogAPI/Categories/views.py
+++ BlogAPI/Categories/views.py
@@ -3,9 +3,11 @@ from requests import request
 from rest_framework.viewsets import ModelViewSet
 from .models import Category
 from .serializers import CategorySerializer
+from .permissions import IsAdminOrReadOnly
 # Create your views here.
 
 class CategoryViewSet(ModelViewSet):
     
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
