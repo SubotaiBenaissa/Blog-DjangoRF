739d2735ad621180a6ffe46515b01bbbdba92447
Alangobe <alangobe01@gmail.com>
2022-07-30 01:32:31 -0500

    Añadido serializadores de user y categorias a post

--- BlogAPI/Post/serializers.py
+++ BlogAPI/Post/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from Post.models import Post
+from Users.serializers import UserSerializer
+from Categories.serializers import CategorySerializer
 
 class PostSerializer(serializers.ModelSerializer):
+    
+    user = UserSerializer()
+    category = CategorySerializer()
     
     class Meta:
         
